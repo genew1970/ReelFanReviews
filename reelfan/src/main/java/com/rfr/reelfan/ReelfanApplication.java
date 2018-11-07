0e1cb7a8b5fe2a4906baad77dc467e26e2b880bb
genew1970 <scor553@gmail.com>
2018-11-07 11:04:04 -0600

    Add files via upload
    initial commit

---
+++ reelfan/src/main/java/com/rfr/reelfan/ReelfanApplication.java
@@ -0,0 +1,12 @@
+package com.rfr.reelfan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReelfanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReelfanApplication.class, args);
+	}
+}
